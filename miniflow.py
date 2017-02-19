916f0d9d396d3a79e876bb2b6ca1e1878934142d
Chandrasekar Sureshkumar <chandrusuresh@gmail.com>
2017-02-19 15:48:23 -0500

    NN with Mean-squared error node.

--- miniflow.py
+++ miniflow.py
@@ -121,6 +121,33 @@ class Sigmoid(Node):
         # if you test without changing this method.
         self.value = self._sigmoid(self.inbound_nodes[0].value)
 
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+            The mean squared error cost function.
+            Should be used as the last node for a network.
+            """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+    
+    def forward(self):
+        """
+            Calculates the mean squared error.
+            """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+        # TODO: your code here
+        self.value = np.mean(np.square(y-a))
+
 def topological_sort(feed_dict):
     """
         Sort generic nodes in topological order using Kahn's Algorithm.
--- nn.py
+++ nn.py
@@ -12,6 +12,26 @@
 from miniflow import *
 import numpy as np
 
+
+def MSE_Example():
+    y, a = Input(), Input()
+    cost = MSE(y, a)
+
+    y_ = np.array([1, 2, 3])
+    a_ = np.array([4.5, 5, 10])
+
+    feed_dict = {y: y_, a: a_}
+    graph = topological_sort(feed_dict)
+    # forward pass
+    forward_pass(cost,graph)
+
+    """
+    Expected output
+    
+    23.4166666667
+    """
+    print(cost.value)
+
 def Sigmoid_Example():
     X, W, b = Input(), Input(), Input()
 
@@ -83,6 +103,7 @@ def Add_Example():
 
 
 if __name__ == "__main__":
-    Sigmoid_Example()
+    MSE_Example()
+#    Sigmoid_Example()
 #    Linear_Example()
 #    Add_Example()
